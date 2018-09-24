e13ca0aee28344c9aa85d255e63aada549f50be2
mohitonet <40100424+mohitonet@users.noreply.github.com>
2018-09-24 10:18:47 +0530

    React form with text area

--- src/index.js
+++ src/index.js
@@ -216,18 +216,20 @@ class NameForm extends React.Component{
     constructor(props)
     {
         super(props);
-        this.state = {value: ''};
+        this.state = {
+            value: 'Please write an essay about your favorite DOM element.'
+          };
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleChange(event){
-        this.setState({value: event.target.value.toUpperCase()});
+        this.setState({value: event.target.value});
     }
 
     handleSubmit(event){
-        alert('A name was subitted: ' + this.state.value);
+        alert('An essay was subitted: ' + this.state.value);
         event.preventDefault();
     }
 
@@ -236,7 +238,7 @@ class NameForm extends React.Component{
             <form onSubmit={this.handleSubmit}>
                 <label>
                     Essay: 
-                    <textarea value = {this.props.value} onChange={this.handleChange} />
+                    <textarea value = {this.state.value} onChange={this.handleChange} />
                 </label>
                 <input type="submit" value="Submit"/>
             </form>
