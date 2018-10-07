ad426e13e6ea09470b25c399992dc2ac35949b54
mohitonet <40100424+mohitonet@users.noreply.github.com>
2018-10-07 11:58:52 +0530

    Composition

--- src/index.js
+++ src/index.js
@@ -409,30 +409,59 @@ function tryConvert(temprature, convert){
     return rounded.toString();
 }
 
-function FancyBorder(props){
+function FancyBorder(props) {
+    return (
+      <div className={'FancyBorder FancyBorder-' + props.color}>
+        {props.children}
+      </div>
+    );
+  }
+
+function Dialog(props){
     return(
-        <div className={'FancyBorder FancyBorder-' + props.color}>
+        <FancyBorder color="blue">
+            <h1 className="Dialog-title">
+                {props.title}
+            </h1>
+            <p className="Dialog-message">
+                {props.message}
+            </p>
             {props.children}
-        </div>
+        </FancyBorder> 
     );
 }
 
-function WelcomeDialog() {
-    return (
-      <FancyBorder color="blue">
-        <h1 className="Dialog-title">
-          Welcome
-        </h1>
-        <p className="Dialog-message">
-          Thank you for visiting our spacecraft!
-        </p>
-      </FancyBorder>
-    );
-  }
-  
+class SignUpDialog extends React.Component{
+    constructor(props){
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSignUp = this.handleSignUp.bind(this);
+        this.state={login: ''};
+    }
+
+    handleChange(e){
+        this.setState({login: e.target.value});
+    }
+
+    handleSignUp(){
+        alert(`Welcome aboard, ${this.state.login}!`);
+    }
+
+    render(){
+        return(
+            <Dialog title="Welcome to signup"
+            message="How should we refer to you?">
+                <input value={this.state.login} onChange={this.handleChange}></input>
+                <button onClick={this.handleSignUp} >
+                    Sign Me Up!
+                </button>
+            </Dialog>
+        );
+    }
+}
 
 ReactDOM.render(
-    <WelcomeDialog />,
+    <SignUpDialog />,
 
     document.getElementById('root')
 );
