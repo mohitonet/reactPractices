35c503c68dc50e8b8541d6e71e7f99f09821de04
mohitonet <40100424+mohitonet@users.noreply.github.com>
2018-10-07 11:32:32 +0530

    Containment

--- src/index.js
+++ src/index.js
@@ -409,8 +409,30 @@ function tryConvert(temprature, convert){
     return rounded.toString();
 }
 
+function FancyBorder(props){
+    return(
+        <div className={'FancyBorder FancyBorder-' + props.color}>
+            {props.children}
+        </div>
+    );
+}
+
+function WelcomeDialog() {
+    return (
+      <FancyBorder color="blue">
+        <h1 className="Dialog-title">
+          Welcome
+        </h1>
+        <p className="Dialog-message">
+          Thank you for visiting our spacecraft!
+        </p>
+      </FancyBorder>
+    );
+  }
+  
+
 ReactDOM.render(
-    <Calculator />,
+    <WelcomeDialog />,
 
     document.getElementById('root')
 );
